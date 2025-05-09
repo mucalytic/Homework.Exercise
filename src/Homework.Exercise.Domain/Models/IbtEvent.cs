3b6eac0fb2a3d41ea07314f2aee5de58929a908b
mucalytic <mucalytic@hotmail.com>
2025-05-09 14:33:30 +0200

    Added more projects and some models.

---
+++ src/Homework.Exercise.Domain/Models/IbtEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Homework.Exercise.Domain.Models;
+
+public class IbtEvent
+{
+    public int      Id        { get; set; }
+    public string   EventType { get; set; } = string.Empty;
+    public DateTime Timestamp { get; set; }
+}
---
+++ src/Homework.Exercise.Domain/Models/IbtMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework.Exercise.Domain.Models;
+
+public record IbtMessage(
+    string   EventType,
+    string   ProductNameFull,
+    string   IbtTypeCode,
+    string   Isin,
+    DateTime Timestamp
+);
