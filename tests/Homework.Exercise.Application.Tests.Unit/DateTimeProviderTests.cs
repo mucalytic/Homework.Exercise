df8dca81f083d2c4ba217dd03efb7bcf5910a719
mucalytic <mucalytic@hotmail.com>
2025-05-12 11:43:30 +0200

    Added some unit tests.

---
+++ tests/Homework.Exercise.Application.Tests.Unit/DateTimeProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using Homework.Exercise.Domain.Interfaces;
+using FluentAssertions;
+using NSubstitute;
+
+namespace Homework.Exercise.Application.Tests.Unit;
+
+public class DateTimeProviderTests
+{
+    [Fact]
+    public void UtcNow_ReturnsCurrentUtcTime()
+    {
+        // Arrange
+        var provider = Substitute.For<IDateTimeProvider>();
+        var expected = new DateTime(2001, 1, 1);
+        provider.UtcNow.Returns(expected);
+        
+        // Act
+        var result = provider.UtcNow;
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
